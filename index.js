b8ab744e69e08df067c32cd004d7958294f86e14
Lescano713 <131562199+Lescano713@users.noreply.github.com>
2024-07-31 02:26:38 -0500

    active state js

--- index.js
+++ index.js
@@ -1,36 +1,84 @@
-const shareAuthor = document.getElementById("share-author");
-const shareSocial = document.getElementById("share-social")
-const divSocialMedia = document.getElementById("social-media");
-const divAuthor = document.getElementById("author-container")
-const mediaQuery = window.matchMedia('(min-width: 800px)');
+// const shareAuthor = document.getElementById("share-author");
+// const shareSocial = document.getElementById("share-social")
+// const divSocialMedia = document.getElementById("social-media");
+// const divAuthor = document.getElementById("author-container");
+const articleContainer = document.getElementById("article")
 
-// function bodys(){
-//     if (body.style.width >= "850px") {
-//         console.log("x")
-//         body.style.backgroundColor = "red";
-//     }else{
-//         console.log("falla");
-//     }
-// }
-// bodys()
+const articles = [{
+    imageAuthor: './images/avatar-michelle.jpg',
+    author: 'Michelle Appleton',
+    date: '28 Jun 2020',
+    imageArticle: './images/drawers.jpg',
+    title: 'Shift the Overall Look and Feel by Adding These Wonderful Touches to Furniture in Your Home',
+    introduction: 'Ever been in a room and felt like something was missing? Perhaps it felt slightly bare and uninviting. I’ve got some simple tips to help you make any room feel complete.',
+    facebook: '#',
+    twitter: '#',
+    pinterest: '#',
+}]
+
+uploadingArticles()
 
 
-shareAuthor.addEventListener("click", showSocialMedia);
+function uploadingArticles(){
+    articles.forEach(article =>{
+        articleContainer.innerHTML = `
+            <div class="img-container">
+                <img class="article-img" src="${article.imageArticle}" alt="article-img">
+            </div>
+            `
+            const infoContainer = document.createElement('div');
+            infoContainer.classList.add('info-container');
+            infoContainer.innerHTML = `
+                <div class="preview-info">
+                    <p class="bolder">${article.title}</b></p>
+                    <p>${article.introduction}</p>
+                </div>
+            `;
+            const authorDiv = document.createElement('div');
+            authorDiv.classList.add('author-container');
+            authorDiv.innerHTML += `
+                    <img class="img-author" src="${article.imageAuthor}">
+                    <div class="author">
+                    <h1>${article.author}</h1>
+                    <p>${article.date}</p>
+                    </div>`
+            const shareI = document.createElement('i');
+            shareI.classList.add("fa-solid");
+            shareI.classList.add("fa-share")
+            shareI.id = "share-author";
+            authorDiv.appendChild(shareI);
 
-function showSocialMedia(){
-    // divAuthor.classList.toggle("hide");
-    divSocialMedia.classList.toggle("show");
+            const socialMediaDiv = document.createElement('div');
+            socialMediaDiv.classList.add('social-media');
+            socialMediaDiv.innerHTML = ` 
+                    <h2>SHARE</h2>
+                    <a href="${article.facebook}" ><img src="./images/icon-facebook.svg" alt="image-author"></a>
+                    <a href="${article.twitter}" ><img src="./images/icon-twitter.svg" alt="image-author"></a>
+                    <a href="${article.pinterest}" ><img src="./images/icon-pinterest.svg" alt="image-author"></a>
+            `;
+            const ShareSocialI = document.createElement('i');
+            ShareSocialI.classList.add("fa-solid");
+            ShareSocialI.classList.add("fa-share");
+            ShareSocialI.id ="share-icon" ;
+            socialMediaDiv.appendChild(ShareSocialI);
+
+            infoContainer.appendChild(authorDiv);
+            infoContainer.appendChild(socialMediaDiv);
+            articleContainer.appendChild(infoContainer);
+
+            shareI.addEventListener('click', e =>{
+                if(window.innerWidth >= 800){
+                    socialMediaDiv.classList.toggle("show");
+                }else{
+                    authorDiv.classList.toggle("hide");
+                    socialMediaDiv.classList.toggle("show");
+                }
+            });
+            
+    })
 }
 
-function changeScreenSize(e){
-    const body = document.body;
-    // if(e.matches){
-    //     body.style.backgroundColor = "red"
-    //     console.log("funciona");
-    // }
-    
-    
-}
-mediaQuery.addListener(changeScreenSize);
+
+
 
 
