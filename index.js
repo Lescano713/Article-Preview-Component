a0966ea672d9009a68fa1d9e99cd6eb87627c8ac
Lescano713 <131562199+Lescano713@users.noreply.github.com>
2024-07-30 23:56:22 -0500

    desktop version

--- index.js
+++ index.js
@@ -2,11 +2,35 @@ const shareAuthor = document.getElementById("share-author");
 const shareSocial = document.getElementById("share-social")
 const divSocialMedia = document.getElementById("social-media");
 const divAuthor = document.getElementById("author-container")
+const mediaQuery = window.matchMedia('(min-width: 800px)');
+
+// function bodys(){
+//     if (body.style.width >= "850px") {
+//         console.log("x")
+//         body.style.backgroundColor = "red";
+//     }else{
+//         console.log("falla");
+//     }
+// }
+// bodys()
+
 
 shareAuthor.addEventListener("click", showSocialMedia);
 
 function showSocialMedia(){
-    divAuthor.classList.toggle("hide");
-    divSocialMedia.classList.toggle("show")
+    // divAuthor.classList.toggle("hide");
+    divSocialMedia.classList.toggle("show");
 }
+
+function changeScreenSize(e){
+    const body = document.body;
+    // if(e.matches){
+    //     body.style.backgroundColor = "red"
+    //     console.log("funciona");
+    // }
+    
+    
+}
+mediaQuery.addListener(changeScreenSize);
+
 
